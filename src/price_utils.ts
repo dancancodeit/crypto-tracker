20745356cb075e540712edcadf0e7906ed23a99a
Daniel Miller <hello@dancancodeit.com>
2025-05-24 02:24:57 -0400

    todo

--- src/price_utils.ts
+++ src/price_utils.ts
@@ -4,4 +4,7 @@ export const usdQuote = 12497
 export const SOL_ADDRESS = 'So11111111111111111111111111111111111111112';
 
 
-export const fetchLargestHolders = () => { }
+//TODO: add static connection?
+export const fetchLargestHolders = async (tokenAddress: string) => {
+        const largestAccounts = (await this.connection.getTokenLargestAccounts(tokenAddress)).value;
+}
